0d9cd34c88284fd3a2b8fd2477e8f4472fd50ff5
pradeep_kumar <pradeep@gravityilabs.com>
2020-10-20 23:34:35 +0530

    register changes

--- _helpers/db.js
+++ _helpers/db.js
@@ -1,7 +1,16 @@
 const config = require('config.json');
 const mongoose = require('mongoose');
-const connectionOptions = { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false };
-mongoose.connect(config.connectionString, connectionOptions);
+//const connectionOptions = { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false };
+const env = require('dotenv').config();
+mongoose.connect("mongodb://"+process.env.COSMOSDB_HOST+":"+process.env.COSMOSDB_PORT+"/"+process.env.COSMOSDB_DBNAME+"?ssl=true&replicaSet=globaldb", {
+  auth: {
+    user: process.env.COSMODDB_USER,
+    password: process.env.COSMOSDB_PASSWORD
+  },
+useNewUrlParser: true,
+useUnifiedTopology: true,
+retryWrites: false
+})
 mongoose.Promise = global.Promise;
 
 module.exports = {
