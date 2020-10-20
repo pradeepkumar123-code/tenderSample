7af00921533e335d58d0b9c3ed6852576216bf93
pradeep_kumar <pradeep@gravityilabs.com>
2020-10-20 21:17:27 +0530

    all files added

---
+++ _helpers/db.js
@@ -0,0 +1,23 @@
+const config = require('config.json');
+const mongoose = require('mongoose');
+const connectionOptions = { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false };
+mongoose.connect("mongodb://"+process.env.COSMOSDB_HOST+":"+process.env.COSMOSDB_PORT+"/"+process.env.COSMOSDB_DBNAME+"?ssl=true&replicaSet=globaldb", {
+  auth: {
+    user: process.env.COSMODDB_USER,
+    password: process.env.COSMOSDB_PASSWORD
+  },
+useNewUrlParser: true,
+useUnifiedTopology: true,
+retryWrites: false
+})
+mongoose.Promise = global.Promise;
+
+module.exports = {
+    Account: require('accounts/account.model'),
+    RefreshToken: require('accounts/refresh-token.model'),
+    isValidId
+};
+
+function isValidId(id) {
+    return mongoose.Types.ObjectId.isValid(id);
+}
