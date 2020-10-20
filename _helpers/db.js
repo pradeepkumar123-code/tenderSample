cbb32507223a93de34657cc10a5c0abbc7e820b6
pradeep_kumar <pradeep@gravityilabs.com>
2020-10-20 22:13:13 +0530

    db config commits

--- _helpers/db.js
+++ _helpers/db.js
@@ -1,15 +1,7 @@
 const config = require('config.json');
 const mongoose = require('mongoose');
 const connectionOptions = { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false };
-mongoose.connect("mongodb://"+process.env.COSMOSDB_HOST+":"+process.env.COSMOSDB_PORT+"/"+process.env.COSMOSDB_DBNAME+"?ssl=true&replicaSet=globaldb", {
-  auth: {
-    user: process.env.COSMODDB_USER,
-    password: process.env.COSMOSDB_PASSWORD
-  },
-useNewUrlParser: true,
-useUnifiedTopology: true,
-retryWrites: false
-})
+mongoose.connect(config.connectionString, connectionOptions);
 mongoose.Promise = global.Promise;
 
 module.exports = {
